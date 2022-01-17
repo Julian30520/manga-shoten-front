51afa74fe1983ab687e6ba7b004f08eff81671b9
Kerkeb Mohamed <kerkeb.moha@gmail.com>
2022-01-17 17:34:10 +0100

    search-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesAccueilComponent } from './pages/pages-accueil/pages-accueil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  {path: 'home', component: PagesAccueilComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
