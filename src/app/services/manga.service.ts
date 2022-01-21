51b027564f0f54cbc3fdf16df3c26f8648ee6fbb
Kerkeb Mohamed <kerkeb.moha@gmail.com>
2022-01-21 15:17:34 +0100

    meilleur mise en page

--- src/app/services/manga.service.ts
+++ src/app/services/manga.service.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { HttpClient} from '@angular/common/http';
-=======
-import { HttpClient } from '@angular/common/http';
->>>>>>> 810bbcb1354c267579bbae5cc4830acef178478a
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -16,18 +12,10 @@ export class MangaService {
   constructor(private httpClient: HttpClient) {}
 
   getAllManga(): Observable<any[]> {
-<<<<<<< HEAD
     return this.httpClient.get<any[]>(`${this.apiUrl}manga/all/30`)
   }
 
   getOneManga(id: any): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.apiUrl}manga/${id}`)
-=======
-    return this.httpClient.get<any[]>(`${this.apiUrl}manga/all/30`);
-  }
-
-  getOneManga(id: any): Observable<any[]> {
-    return this.httpClient.get<any[]>(`${this.apiUrl}manga/${id}`);
->>>>>>> 810bbcb1354c267579bbae5cc4830acef178478a
   }
 }
